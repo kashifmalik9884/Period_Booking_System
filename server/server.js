a99a1463f0c4089f8495580e62831e948beaac88
MOHD KASHIF <76276260+kashifmalik9884@users.noreply.github.com>
2026-07-30 13:11:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-site-name.netlify.app",
+      "https://period-booking-system.netlify.app",
     ],
     credentials: true,
   })
