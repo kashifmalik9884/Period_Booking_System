e3af467187ab7a7036e880dba46e79f7a38c4478
MOHD KASHIF <76276260+kashifmalik9884@users.noreply.github.com>
2026-07-30 13:16:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const authRoutes = require("./routes/authRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
 const historyRoutes = require("./routes/historyRoutes");
 
-const cors = require("cors");
+const app = express();
 
 app.use(
   cors({
@@ -18,6 +18,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
